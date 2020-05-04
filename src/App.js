83bf9ca5e248263922d660d3ca25b4e077f43316
xianengqi <xianengqi@gmail.com>
2020-05-04 19:11:38 +0800

    app.js add state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import SimpleMDE from "react-simplemde-editor";
@@ -12,27 +12,35 @@ import BottomBtn from './components/BottomBtn'
 import TableList from './components/TableList'
 
 function App() {
+  const [files, setFiles] = useState(defaultFiles)
+  const [activeFileID, setActiveFileID] = useState('')
+  const [openedFileIDs, setopenedFileIDs] = useState([])
+  const [unsaveFileIDs, setunsaveFileIDs] = useState([])
+  const openedFiles = openedFileIDs.map(openID => {
+    return files.find(file => file.id === openID)
+  })
+  const activeFile = files.find(file => file.id === activeFileID)
   return (
     <div className="App container-fluid px-0">
       <div className="row no-gutters">
         <div className="col-3 bg-light left-panel">
           <FileSearch
             title="我的云文档"
-            onFileSearch={(value) => {console.log(value) }}
+            onFileSearch={(value) => { console.log(value) }}
           />
           <FileList
-            files={defaultFiles}
-            onFileClick={(id) => {console.log('click => ', id);}}
-            onFileDelete={(id) => {console.log('delete =>', id);}}
-            onFileEdit={(id, newValue) => {console.log('edit => ', id, newValue);}}
+            files={files}
+            onFileClick={(id) => { console.log('click => ', id); }}
+            onFileDelete={(id) => { console.log('delete =>', id); }}
+            onFileEdit={(id, newValue) => { console.log('edit => ', id, newValue); }}
           />
-          <div className="row no-gutters">
+          <div className="row no-gutters button-group">
             <div className="col">
               <BottomBtn
                 text="新建"
                 colorClass="btn-primary"
                 icon={faPlus}
-                onBtnClick={(e) => {console.log('onBtn =>', e);}}
+                onBtnClick={(e) => { console.log('onBtn =>', e); }}
               />
             </div>
             <div className="col">
@@ -40,26 +48,33 @@ function App() {
                 text="导入"
                 colorClass="btn-success"
                 icon={faFileImport}
-                onBtnClick={(e) => {console.log('onBtnImport =>', e);}}
+                onBtnClick={(e) => { console.log('onBtnImport =>', e); }}
               />
             </div>
           </div>
         </div>
         <div className="col-9 right-panel">
-          <TableList
-            files={defaultFiles}
-            unsaveIds={["1", "2"]}
-            onTabClick={(id) => {console.log('tabList => ', id);}}
-            onCloseTab={(id) => {console.log('Close => ', id);}}
-            activeId="1"
-          />
-          <SimpleMDE
-            value={defaultFiles[0].body}
-            onChange={(value) => {console.log('markdown =>', value);}}
-            options={{
-              minHeight: '515px'
-            }}
-          />
+          {
+            !activeFile && <div className="start-page">选择或者创建新的 markdown 文档</div>
+          }
+          { activeFile &&
+            <>
+              <TableList
+                files={openedFiles}
+                unsaveIds={unsaveFileIDs}
+                onTabClick={(id) => { console.log('tabList => ', id); }}
+                onCloseTab={(id) => { console.log('Close => ', id); }}
+                activeId={activeFileID}
+              />
+              <SimpleMDE
+                value={activeFile && activeFile.body}
+                onChange={(value) => { console.log('markdown =>', value); }}
+                options={{
+                  minHeight: '515px'
+                }}
+              />
+            </>
+          }
         </div>
       </div>
     </div>
